7c7a324171a7ebe378f5cb1c6260517a26550aed
Anderson Cesar Malveira Pinheiro <andersonc_pinheiro@outlook.com>
2022-10-26 21:27:46 -0300

    add project react

---
+++ react_projects/discovery/reactapp/readme.txt
@@ -0,0 +1,12 @@
+Criando projeto em react
+
+Mkdir para criar nova pasta para instalar o projeto.
+
+Comando para criar o projeto React
+npm create vite@latest reactapp --template react
+
+Instalar o node_moduls
+npm install
+
+Iniciar o projeto react
+npm run dev
---
+++ react_projects/discovery/reactapp/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import './styles.css'
+function Home() {
+ 
+  return(
+    <div className="container">
+    <h1>Lista de presença</h1>
+    <input type="text" placeholder="Digite o nome"/>
+    <button type="button">Adicionar</button>
+    </div>
+  )
+}
+
+export default Home

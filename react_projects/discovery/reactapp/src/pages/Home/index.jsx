5c9f75de54b4f71ecf10727c5475bd7775667124
Anderson Cesar Malveira Pinheiro <andersonc_pinheiro@outlook.com>
2022-10-28 06:32:54 -0300

    include jsx archives

--- react_projects/discovery/reactapp/src/pages/Home/index.jsx
+++ react_projects/discovery/reactapp/src/pages/Home/index.jsx
@@ -1,12 +1,47 @@
+//Uso do useState, garante que o conteúdo da variavel irá refletir 
+//na tela além de ser armazenado em uma variavel
+import React, {useState}from 'react';
+
 import './styles.css'
+import {Card} from '../../components/Card';
+
 function Home() {
- 
+  const[studentName, setStudentName] = useState('');
+  const[students, setStudents]=useState([]);
+
+function handleStudent(){
+const newStudent ={
+  name: studentName,
+  time: new Date().toLocaleTimeString("pt-br",{
+    hour: '2-digit',
+    minute:'2-digit',
+    second:'2-digit'
+  })
+};
+
+setStudents( prevState =>[...prevState, newStudent]);
+
+}  
+
   return(
     <div className="container">
     <h1>Lista de presença</h1>
-    <input type="text" placeholder="Digite o nome"/>
-    <button type="button">Adicionar</button>
+    
+    <input 
+    type="text" 
+    placeholder="Digite o nome"
+    onChange={ e => setStudentName(e.target.value)}
+    />
+    
+    <button type="button" onClick={handleStudent}>
+      Adicionar</button>
+    
+    {
+    students.map(student => <Card name={student.name} time={student.time}/> )    
+    
+    }
     </div>
+
   )
 }
 

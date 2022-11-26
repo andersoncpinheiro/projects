8900371ddb549931e6f2b068f880e48c982c1eae
Anderson Cesar Malveira Pinheiro <andersonc_pinheiro@outlook.com>
2022-11-26 08:30:20 -0300

    incluida atualizacao para direcionar usuario a pagina de cadastro

--- sistema-estoque/app/routes/admin.js
+++ sistema-estoque/app/routes/admin.js
@@ -1,8 +1,16 @@
 module.exports = function(app){
 
 app.get ('/admin',function(req,res){
+  //  var connection = app.config.dbConnection();       
+   // connection.query('select * from produto where id_produto = 1', function(error, result){
+    //original
     res.render("admin/admin");
-});
+    //original
+   // res.render("admin/admin", {admin: result});
+    });
+
+//});
+
 app.post ('/produtos/salvar',function(req,res){
     var produtos = req.body;
 
@@ -10,11 +18,8 @@ app.post ('/produtos/salvar',function(req,res){
     var produtosModel = app.app.models.produtosModel;
 
    produtosModel.salvarProdutos(produtos, connection, function(error, result){
-        res.redirect('/produtos')
+        res.redirect('/admin')
         });
 
     });
-
-   
-
 }

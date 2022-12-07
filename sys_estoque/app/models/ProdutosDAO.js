f898b84ee1ffe372e9510a0ac5c54a9d4c463ac0
Anderson Cesar Malveira Pinheiro <andersonc_pinheiro@outlook.com>
2022-12-07 15:05:58 -0300

    add new version sys_estoque

--- sys_estoque/app/models/ProdutosDAO.js
+++ sys_estoque/app/models/ProdutosDAO.js
@@ -3,11 +3,11 @@ function ProdutosDAO(connection){
 }
 
 ProdutosDAO.prototype.getProduto = function(callback){
-    this._connection.query('SELECT * FROM produto', callback);
+    this._connection.query('select * from produto p where p.id_produto = (select max(p.id_produto) from produto p', callback);
 }
 
 ProdutosDAO.prototype.getProdutos = function(callback){
-    this._connection.query('select * from produto p where p.id_produto = (select max(p.id_produto) from produto p)', callback);
+    this._connection.query('select * from produto', callback);
 }
 
 ProdutosDAO.prototype.salvarProduto = function(produto, callback){

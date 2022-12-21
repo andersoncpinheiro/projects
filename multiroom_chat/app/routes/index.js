dc8925235e75494356fbd4445538ce960dee42c2
Anderson Cesar Malveira Pinheiro <andersonc_pinheiro@outlook.com>
2022-12-21 09:24:30 -0300

    adicionado projeto multiroom e criadas rotas

--- multiroom_chat/app/routes/index.js
+++ multiroom_chat/app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function(application){
-    application.get('/', function (req, res){
-    res.send('teste')
+application.get('/', function (req, res){
+//excutar função que está sendo exportada em index no controller.
+    application.app.controllers.index.home(application, req, res)
 });
 }

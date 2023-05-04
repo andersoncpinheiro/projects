0b6f960ce588ad6aa048dfd7b201671660f9e9b3
Anderson Malveira <anderson.malveira@detran.ce.gov.br>
2023-05-04 16:46:06 -0300

    adicionado refatoramento de codigo de estudo de hash

--- curso-rails_codes/Projeto_teste/hash_gets.rb
+++ curso-rails_codes/Projeto_teste/hash_gets.rb
@@ -1,15 +1,30 @@
-userhash=Hash.new()
+def cadastrar_aluno
+
+    userhash=Hash.new()
+        
+    puts "Informe o nome do titulo do campo1"
+        @field1 = gets.chomp
+
+    puts "Informe o nome do titulo do campo2"
+        @field2 = gets.chomp
+
+    puts "Informe o nome do titulo do campo3"
+         @field3 = gets.chomp
+
+
+    puts "Informe o "+@field1 +" do aluno"
+        userhash[@field1]=gets.chomp
+
+    puts "Informe a "+@field2 +" do aluno"
+        userhash[@field2]=gets.chomp.to_i
+
+    puts "Informe o "+@field3 +" do aluno M ou F"
+        userhash[@field3]=gets.chomp
+    
+        puts userhash
 
 
 
+end
 
-puts "Enter first number"
-userhash[:num1]=gets.chomp.to_f
-puts "Enter 2nd number"
-userhash[:num2]=gets.chomp.to_f
-puts "Enter 3rd number"
-userhash[:num3]=gets.chomp.to_f
-puts userhash
-
-
-
+cadastrar_aluno

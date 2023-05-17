491ff7ac4e758c5566a2e18971e734169f66cc83
anderson <andersonc_pinheiro@outlook.com>
2023-05-16 21:40:40 -0300

    add final

--- blog/blogstrep/app/controllers/articles_controller.rb
+++ blog/blogstrep/app/controllers/articles_controller.rb
@@ -4,14 +4,14 @@ class ArticlesController < ApplicationController
 
   
   def index
-    @highlights = Article.order(created_at: :desc).first(3)
+    @highlights = Article.desc_order.first(3)
     
     current_page = (params[:page] || 1).to_i
     highlight_ids = @highlights.pluck(:id).join(',')
     
     #gem kaminari(PAGINACAO) permite visualizar 2 registros por pagina
-    @articles = Article.order(created_at: :desc)
-    .where("id NOT IN(#{highlight_ids})")
+    @articles = Article.without_highlights(highlight_ids)
+    .desc_order
     .page(current_page).per(2)
   end
   def show
--- blog/blogstrep/app/models/article.rb
+++ blog/blogstrep/app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: {minimum:10, maximum:30}
     validates :body, presence: true, length: {minimum:10, maximum:100}
+    
+    scope :desc_order, -> {order(created_at: :desc)}
+    scope :without_highlights, -> (ids) {where("id NOT IN(#{ids})")}
 
 end

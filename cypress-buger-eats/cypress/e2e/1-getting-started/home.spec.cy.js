44628e8acd10317bcab92370c707316a0bdbf4d3
Anderson Pinheiro <110304818+andersoncpinheiro@users.noreply.github.com>
2023-08-17 14:35:56 -0300

    delete
    delete

--- cypress-buger-eats/cypress/e2e/1-getting-started/home.spec.cy.js
+++
@@ -1,8 +0,0 @@
-describe('home page', ()=>{
-  it ('app deve estar online', ()=>{
-    cy.viewport(1440, 1080) 
-    cy.visit('https://buger-eats.vercel.app')
-    //should função que valida, como parametro deve ter um texto 
-    cy.get('#page-home main h1').should('have.text','Seja um parceiro entregador pela Buger Eats' )
-  }) 
-})

80f9c243ce1ba4fda283e6dbda83f1a6d1e0b7b0
Anderson Malveira <anderson.malveira@detran.ce.gov.br>
2024-03-22 16:16:19 -0300

    feat/adicionado inclusao de admin e update

--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+ 
   namespace :site do
     get 'welcome/index'
   end 
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get 'welcome/index' #Dashboard
+    resources :admins, except: [:delete] #Administradores
   end
   namespace :users_backoffice do
     get 'welcome/index'

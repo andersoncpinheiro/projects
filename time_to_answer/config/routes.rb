a0698818062babf9a36eaeb089d46eac3142ff66
Anderson Malveira <anderson.malveira@detran.ce.gov.br>
2024-03-26 15:40:47 -0300

    feat/adicionado paginacao com kaminari

--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   end 
   namespace :admins_backoffice do
     get 'welcome/index' #Dashboard
-    resources :admins, except: [:delete] #Administradores
+    resources :admins #Administradores
   end
   namespace :users_backoffice do
     get 'welcome/index'

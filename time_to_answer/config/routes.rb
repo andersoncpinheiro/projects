ba65a95bfc1d4361bd5215bb814fe21416cfc626
Anderson Malveira <anderson.malveira@detran.ce.gov.br>
2024-04-18 08:50:28 -0300

    feat/inclusao_controller_questions_views_modelo

--- time_to_answer/config/routes.rb
+++ time_to_answer/config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   namespace :admins_backoffice do
     get 'welcome/index' #Dashboard
     resources :admins #Administradores
+    resources :subjects #Assuntos/Áreas
+    resources :questions #Perguntas
   end
   namespace :users_backoffice do
     get 'welcome/index'

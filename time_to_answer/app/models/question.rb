61c46d174faeac1f022bfeddc57a371188020e98
Anderson Malveira <anderson.malveira@detran.ce.gov.br>
2024-05-16 11:16:21 -0300

    feat/ adicionado label para contagem de subjects

--- time_to_answer/app/models/question.rb
+++ time_to_answer/app/models/question.rb
@@ -5,14 +5,19 @@ class Question < ApplicationRecord
 
   paginates_per 5 #kaminari
 
-  scope :_search_, ->(page, term){
-    includes(:answers)
-            .where("lower(description)  LIKE ?", "%#{term.downcase}%")
+  scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject)
+            .where(subject_id: subject_id)
             .page(page)
           }
- 
+
+  scope :_search_, ->(page, term){
+    includes(:answers, :subject)
+            .where("lower(description)  LIKE ?", "%#{term.downcase}%")
+            .page(page)
+          } 
 
   scope :last_questions, ->(page){
-    includes(:answers).order('created_at desc').page(page)
+    includes(:answers, :subject).order('created_at desc').page(page)
         }
 end
